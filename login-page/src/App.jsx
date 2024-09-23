c0712e4a0ac646629711d7e0b94d85fcdc35cb3d
mohammed kaif <mkaif555123@gmail.com>
2024-09-23 23:58:56 +0500

    10% Work Completed

--- login-page/src/App.jsx
+++ login-page/src/App.jsx
@@ -1,14 +1,18 @@
-import './App.css'
+import React from 'react';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-
   return (
     <>
-    <div className="bg-white p-9 w-80 mx-auto my-44">
-   <h1>Hello World</h1>
-    </div>
+      <div className="bg-white p-7 w-96 mx-auto my-44 rounded-md">
+        <div className='text-center text-3xl'> 
+          <FontAwesomeIcon icon={faTwitter} style={{ color: '#33beff' }} />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
